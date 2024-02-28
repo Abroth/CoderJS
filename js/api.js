68a2874525e3169c33c4cdc1bbcc21befdeed812
Juan Ignacio Robert <jirobert1995@gmail.com>
2024-02-27 23:43:45 -0300

    amplicacion de weather a la pagina principal

--- js/api.js
+++ js/api.js
@@ -5,6 +5,7 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
+const weatherTemp = document.querySelector(".temp");
 
 async function checkWeather(city){
     const response = await fetch(apiURL + city +  `&appid=${apiKey}`);
@@ -14,9 +15,9 @@ async function checkWeather(city){
     console.log(data)
 
     document.querySelector(".city").innerHTML = data.name
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°c"
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
-    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h"
+    weatherTemp.innerHTML = Math.round(data.main.temp)  + "°c"
+    // document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
+    // document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h"
 
     if(data.weather[0].main == "Clouds"){
         weatherIcon.src = "img/clouds.png"
@@ -29,10 +30,12 @@ async function checkWeather(city){
     }else if (data.weather[0].main == "Mist"){
         weatherIcon.src = "img/mist.png"
     }
+
 }
 
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value)
 })
+
 
 
